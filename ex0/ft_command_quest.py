1bd7247fe7c379dcd07a6f0ee9c2d47cb693328e
saydo20 <jdiasaad86@gmail.com>
2026-01-16 12:15:18 +0100

    test

---
+++ ex0/ft_command_quest.py
@@ -0,0 +1,12 @@
+import sys
+print("=== Command Quest ===")
+if len(sys.argv) <= 1:
+    print("No arguments provided!")
+else:
+    print(f"Program name: {sys.argv[0]}")
+    print(f"Arguments received: {len(sys.argv) - 1}")
+    i = 1
+    while i < len(sys.argv):
+        print(f"Argument {i}: {sys.argv[i]}")
+        i += 1
+print(f"Total arguments: {len(sys.argv)}")
---
+++ ex1/ft_score_analytics.py
@@ -0,0 +1,25 @@
+import sys
+
+print("=== Player Score Analytics ===")
+if len(sys.argv) <= 1:
+    print("No scores provided. Usage:"
+          " python3 ft_score_analytics.py <score1> <score2> ...")
+else:
+    try:
+        list = []
+        i = 1
+        while i < len(sys.argv):
+            list = list + [int(sys.argv[i])]
+            i += 1
+        print(f"Scores processed: {list}")
+        print(f"Total players: {len(sys.argv) - 1}")
+        total = 0
+        for number in list:
+            total += number
+        print(f"Total score: {total}")
+        print(f"Average score: {total / (len(sys.argv) - 1)}")
+        print(f"High score: {max(list)}")
+        print(f"Low score: {min(list)}")
+        print(f"Score range: {max(list) - min(list)}\n")
+    except ValueError:
+        print("invalid scores")
